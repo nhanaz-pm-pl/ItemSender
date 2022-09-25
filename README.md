516b771a970a957b59c134983cc966d4ba5b4677
Thành Nhân <60387689+NhanAZ@users.noreply.github.com>
2022-09-25 07:11:17 +0700

    Stop hardcode strings

--- README.md
+++ README.md
@@ -1,16 +1,19 @@
 # General
 **Sending the item in hand to someone with /itemsender command**
 
-# Commands
-
-| Commands | Description | Usage | Aliases | Permission | Permission Message |
-| -------- | ----------- | ----- | ------- | ---------- | ------------------ |
-| /itemsender | Sending the item in hand to someone | /itemsender \<player> \<amount> | ["/itemsend"] | itemsender.command | N/A |
-
-# Permissions
-| Permissions | Description | Default |
-| ----------- | ----------- | ------- |
-| itemsender.command | Permission for /itemsender | true |
+# Commands & Permissions
+```yaml
+commands:
+  itemsender:
+    description: Sending the item in hand to someone
+    usage: "/itemsender <player> <amount>"
+    aliases: [itemsend]
+    permission: itemsender.command
+permissions:
+  itemsender.command:
+    description: Permission for /itemsender
+    default: true
+```
 
 # Contact
 [![Discord](https://img.shields.io/discord/986553214889517088?label=discord&color=7289DA&logo=discord)](https://discord.gg/j2X83ujT6c)\
--- src/NhanAZ/ItemSender/Main.php
+++ src/NhanAZ/ItemSender/Main.php
@@ -9,67 +9,69 @@ use pocketmine\command\CommandSender;
 use pocketmine\plugin\PluginBase;
 use pocketmine\player\Player;
 use pocketmine\network\mcpe\protocol\PlaySoundPacket;
-USE pocketmine\item\VanillaItems;
+use pocketmine\item\VanillaItems;
+use pocketmine\utils\TextFormat;
 
 class Main extends PluginBase {
 
-	// TODO: Stop hardcode strings
+	private function playSound(Player $player, string $soundName) {
+		$playerPos = $player->getPosition();
+		$player->getNetworkSession()->sendDataPacket(PlaySoundPacket::create($soundName, $playerPos->getX(), $playerPos->getY(), $playerPos->getZ(), 1, 1));
+	}
 
 	public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool {
 		if ($command->getName() === "itemsender") {
 			if (!$sender instanceof Player) {
-				$sender->sendMessage("§cYou can't use this command in the terminal!");
+				$sender->sendMessage(TextFormat::colorize($this->getConfig()->get("cmdInTerminal")));
 				return true;
 			}
-			$senderPos = $sender->getPosition();
 			if (!isset($args[0]) || !isset($args[1])) {
-				$sender->sendMessage("§cPlease do not leave arguments blank!");
-				$sender->getNetworkSession()->sendDataPacket(PlaySoundPacket::create("mob.villager.no", $senderPos->getX(), $senderPos->getY(), $senderPos->getZ(), 1, 1));
+				$sender->sendMessage(TextFormat::colorize($this->getConfig()->get("argumentsBlank")));
+				$this->playSound($sender, "mob.villager.no");
 				return false;
 			}
-			$player = $this->getServer()->getPlayerByPrefix($args[0]);
-			if (!$player) {
-				$sender->sendMessage("§cThe player with the name §b$args[0] §adoes not exist or is not online.");
-				$sender->getNetworkSession()->sendDataPacket(PlaySoundPacket::create("mob.villager.no", $senderPos->getX(), $senderPos->getY(), $senderPos->getZ(), 1, 1));
+			$target = $this->getServer()->getPlayerByPrefix($args[0]);
+			if (!$target) {
+				$sender->sendMessage(TextFormat::colorize(str_replace("{target}", $args[0], $this->getConfig()->get("invalidTarget"))));
+				$this->playSound($sender, "mob.villager.no");
 				return true;
 			}
 			if (!is_numeric($args[1]) || $args[1] < 1) {
-				$sender->sendMessage("§b<amount>§c must be numeric and greater than §b0");
-				$sender->getNetworkSession()->sendDataPacket(PlaySoundPacket::create("mob.villager.no", $senderPos->getX(), $senderPos->getY(), $senderPos->getZ(), 1, 1));
+				$sender->sendMessage(TextFormat::colorize($this->getConfig()->get("invalidInput")));
+				$this->playSound($sender, "mob.villager.no");
 				return true;
 			}
-			if ($sender == $player) {
-				$sender->sendMessage("§cYou can't send things to yourself");
-				$sender->getNetworkSession()->sendDataPacket(PlaySoundPacket::create("mob.villager.no", $senderPos->getX(), $senderPos->getY(), $senderPos->getZ(), 1, 1));
+			if ($sender == $target) {
+				$sender->sendMessage(TextFormat::colorize($this->getConfig()->get("sendYourself")));
+				$this->playSound($sender, "mob.villager.no");
 				return true;
 			}
-			$playerName = $player->getName();
+			$targetName = $target->getName();
 			$item = $sender->getInventory()->getItemInHand();
 			$itemName = $item->getName();
 			$itemCount = $item->getCount();
 			if ($item->equals(VanillaItems::AIR())) {
 				// Call to an undefined static method pocketmine\item\VanillaItems::AIR().
-				$sender->sendMessage("§cYou can't send §b$itemName §cto §b$playerName");
-				$sender->getNetworkSession()->sendDataPacket(PlaySoundPacket::create("mob.villager.no", $senderPos->getX(), $senderPos->getY(), $senderPos->getZ(), 1, 1));
+				$sender->sendMessage(TextFormat::colorize(str_replace(["{air}", "{target}"], [$itemName, $targetName], $this->getConfig()->get("sendAir"))));
+				$this->playSound($sender, "mob.villager.no");
 				return true;
 			}
 			if ($item->getCount() < $args[1]) {
-				$sender->sendMessage("§cYou don't have enough §b$args[1] $itemName §cin hand to send §b$playerName");
-				$sender->getNetworkSession()->sendDataPacket(PlaySoundPacket::create("mob.villager.no", $senderPos->getX(), $senderPos->getY(), $senderPos->getZ(), 1, 1));
+				$sender->sendMessage(TextFormat::colorize(str_replace(["{itemCount}", "{itemName}", "{target}"], [$itemCount, $itemName, $targetName], $this->getConfig()->get("notEnoughItems"))));
+				$this->playSound($sender, "mob.villager.no");
 				return true;
 			}
-			if (!$player->getInventory()->canAddItem($item->setCount((int) $args[1]))) {
-				$sender->sendMessage("§b$playerName's §cinventory doesn't have enough space!");
-				$sender->getNetworkSession()->sendDataPacket(PlaySoundPacket::create("mob.villager.no", $senderPos->getX(), $senderPos->getY(), $senderPos->getZ(), 1, 1));
+			if (!$target->getInventory()->canAddItem($item->setCount((int) $args[1]))) {
+				$sender->sendMessage(TextFormat::colorize(str_replace("{target}", $targetName, $this->getConfig()->get("notEnoughSpace"))));
+				$this->playSound($sender, "mob.villager.no");
 				return true;
 			}
 			$sender->getInventory()->removeItem($item->setCount((int) $args[1]));
-			$player->getInventory()->addItem($item->setCount((int) $args[1]));
-			$sender->sendMessage("§aSent §b$itemCount $itemName §ato §b$playerName");
-			$sender->getNetworkSession()->sendDataPacket(PlaySoundPacket::create("mob.villager.yes", $senderPos->getX(), $senderPos->getY(), $senderPos->getZ(), 1, 1));
-			$player->sendMessage("§aYou received §b$itemCount $itemName §afrom §b$playerName");
-			$playerPos = $player->getPosition();
-			$player->getNetworkSession()->sendDataPacket(PlaySoundPacket::create("mob.villager.yes", $playerPos->getX(), $playerPos->getY(), $playerPos->getZ(), 1, 1));
+			$target->getInventory()->addItem($item->setCount((int) $args[1]));
+			$sender->sendMessage(TextFormat::colorize(str_replace(["{itemCount}", "{itemName}", "{target}"], [$args[1], $itemName, $targetName], $this->getConfig()->get("sentSuccessfully"))));
+			$this->playSound($sender, "mob.villager.yes");
+			$target->sendMessage(TextFormat::colorize(str_replace(["{itemCount}", "{itemName}", "{target}"], [$args[1], $itemName, $targetName], $this->getConfig()->get("receivedSuccessfully"))));
+			$this->playSound($target, "mob.villager.yes");
 			return true;
 		}
 		return false;
